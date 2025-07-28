4a4527fd0be4b3da7fafacf18380c97c0c41d2e5
William-SGV <garnicavaldez9999@gmail.com>
2025-07-27 21:38:53 -0600

    script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-console.log("¡Bienvenido a Chocolates William!");
-
-
-let contador = 0;
-
-function incrementarContador() {
-    contador++;
-    document.getElementById("contador").textContent = contador;
-}
